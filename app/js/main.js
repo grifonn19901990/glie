a9f47f0305b0a39451a165d641d8bfb3eda49aa0
grifonn19901990 <grifonn19901990@gmail.com>
2023-09-06 16:35:21 +0300

    added product-list1 and product-list2 pages

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,61 @@
 $(function () {
+  $('.filter__price-input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data){
+      $('.filter__price-from').text(data.from);
+      $('.filter__price-to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter__price-from').text(data.from);
+      $('.filter__price-to').text(data.to);
+    },
+  });
+
+  $('.content2__item-like1').on('click', function () {
+    $('.catalog__content2-item1').addClass('catalog__content2-item--like');
+  });
+  $('.catalog__content2-item-btn1').on('click', function () {
+    $('.catalog__content2-item1').removeClass('catalog__content2-item--like');
+  });
+  $('.content2__item-like2').on('click', function () {
+    $('.catalog__content2-item2').addClass('catalog__content2-item--like');
+  });
+  $('.catalog__content2-item-btn2').on('click', function () {
+    $('.catalog__content2-item2').removeClass('catalog__content2-item--like');
+  });
+  $('.content2__item-like3').on('click', function () {
+    $('.catalog__content2-item3').addClass('catalog__content2-item--like');
+  });
+  $('.catalog__content2-item-btn3').on('click', function () {
+    $('.catalog__content2-item3').removeClass('catalog__content2-item--like');
+  });
+  $('.content2__item-like4').on('click', function () {
+    $('.catalog__content2-item4').addClass('catalog__content2-item--like');
+  });
+  $('.catalog__content2-item-btn4').on('click', function () {
+    $('.catalog__content2-item4').removeClass('catalog__content2-item--like');
+  });
+
+
+  $('.recent__products-star').rateYo({
+    
+    starWidth: "10px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffd633",
+    readOnly: true
+   
+  });
+
+  $('.catalog__content2-item-star').rateYo({
+
+    starWidth: "18px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffd633",
+    readOnly: true
+
+  });
+
   $('.top__slider-inner').slick({
     dots: true,
     arrows: false,
@@ -18,3 +75,4 @@ $(function () {
   var mixer = mixitup('.desing__content');
 
 });
+
