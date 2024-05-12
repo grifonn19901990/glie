02803190eb037091c6cb2075b8b4618d06a7f19f
grifonn19901990 <grifonn19901990@gmail.com>
2024-05-12 11:54:33 +0300

    added contact page and fixed about page

--- app/js/main.js
+++ app/js/main.js
@@ -113,4 +113,31 @@ $(function () {
   });
 
 });
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: 29.773238276799987, lng: -95.32314672356658 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 9,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerView({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
+}
+
+initMap();
+
 
